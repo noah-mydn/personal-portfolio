8b6f1eb5e2b1273aa9818d2401c7e36485d4a70f
noah-mydn <mayyadanar@hotmail.com>
2023-06-02 11:37:55 +0630

    fixed project orders

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,16 @@
 export const PROJECTS = [
   {
     id: 1,
+    title: "BEAST - Real Time Chat App",
+    desktop_image: "./assets/beast_2.png",
+    phone_image: "./assets/beast.png",
+    codeLang: "MERN stack + sockets",
+    framework: ["Bootstrap"],
+    demoLink: "https://beast-chat.onrender.com/",
+    repoLink: "https://github.com/noah-mydn/beast",
+  },
+  {
+    id: 2,
     title: "Hera - Ecommerce Site",
     desktop_image: "./assets/hera_2.png",
     phone_image: "./assets/hera.png",
@@ -9,18 +19,6 @@ export const PROJECTS = [
     api: "Makeup API",
     demoLink: "https://hera-cosmetics.netlify.app/",
     repoLink: "https://github.com/noah-mydn/cosmetic-ecommerce-hera",
-  },
-
-  {
-    id: 2,
-    title: "Apollo - Music Streaming Site",
-    desktop_image: "./assets/apollo.png",
-    phone_image: "./assets/apollo_2.png",
-    codeLang: "React",
-    framework: ["MUI"],
-    api: "Spotify API",
-    demoLink: "https://apollo-noah.netlify.app",
-    repoLink: "https://github.com/noah-mydn/apollo",
   },
 
   {
@@ -37,12 +35,13 @@ export const PROJECTS = [
 
   {
     id: 4,
-    title: "BEAST - Real Time Chat App",
-    desktop_image: "./assets/beast_2.png",
-    phone_image: "./assets/beast.png",
-    codeLang: "MERN stack + sockets",
-    framework: ["Bootstrap"],
-    demoLink: "https://beast-chat.onrender.com/",
-    repoLink: "https://github.com/noah-mydn/beast",
+    title: "Apollo - Music Streaming Site",
+    desktop_image: "./assets/apollo.png",
+    phone_image: "./assets/apollo_2.png",
+    codeLang: "React",
+    framework: ["MUI"],
+    api: "Spotify API",
+    demoLink: "https://apollo-noah.netlify.app",
+    repoLink: "https://github.com/noah-mydn/apollo",
   },
 ];
